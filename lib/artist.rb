8fea605f795dcaee200d5b7de5b03f00649de7bc
“Abbie <abbie.coghlan@gmail.com>
2020-12-13 15:56:17 -0600

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,32 @@
+require "pry"
 
+class Artist 
+    
+    attr_accessor :name, :genre
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end 
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end 
+    
+
+    def genres
+        self.songs.collect {|song| song.genre}
+    end 
+   
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+
+end
